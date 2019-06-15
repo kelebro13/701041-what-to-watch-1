dfb1a68d0299d0dcfc64042cea9b153b2baa37d4
TimGum <tgumerov@gmail.com>
2019-06-15 16:28:42 +0500

    Добавил тесты для видео плеера

--- src/components/video-player/video-player.e2e.test.jsx
+++ src/components/video-player/video-player.e2e.test.jsx
@@ -1,3 +1,77 @@
-it(``, () => {
+import {mount} from "enzyme";
+import VideoPlayer from "./video-player";
 
+it(`should open full screen if click on button`, () => {
+  const video = {
+    onloadeddata: null,
+    ontimeupdate: null,
+    play: jest.fn(),
+    pause: jest.fn(),
+    requestFullscreen: jest.fn()
+  };
+
+  const videoPlayer = mount(<VideoPlayer name={`Test`} poster={``}/>);
+  videoPlayer.instance().videoRef.current = video;
+
+  videoPlayer.find(`.player__full-screen`).simulate(`click`);
+  expect(video.requestFullscreen).toHaveBeenCalledTimes(1);
+});
+
+it(`should stop video and call onExit if click exit button`, () => {
+  const video = {
+    onloadeddata: null,
+    ontimeupdate: null,
+    play: jest.fn(),
+    pause: jest.fn(),
+    requestFullscreen: jest.fn()
+  };
+
+  const onExit = jest.fn();
+
+  const videoPlayer = mount(<VideoPlayer name={`Test`} poster={``} onExit={onExit}/>);
+  videoPlayer.instance().videoRef.current = video;
+
+  videoPlayer.find(`.player__exit`).simulate(`click`);
+  expect(video.pause).toHaveBeenCalledTimes(1);
+  expect(onExit).toHaveBeenCalledTimes(1);
+});
+
+it(`should play video if click play button`, () => {
+  const video = {
+    onloadeddata: null,
+    ontimeupdate: null,
+    play: jest.fn(),
+    pause: jest.fn(),
+    requestFullscreen: jest.fn()
+  };
+
+  const onExit = jest.fn();
+  const onActiveStatusChange = jest.fn();
+
+  const videoPlayer = mount(<VideoPlayer name={`Test`} poster={``} onExit={onExit} isActive={false} onActiveStatusChange={onActiveStatusChange}/>);
+  videoPlayer.instance().videoRef.current = video;
+
+  videoPlayer.find(`.player__play`).simulate(`click`);
+  expect(video.play).toHaveBeenCalledTimes(1);
+  expect(onActiveStatusChange).toHaveBeenCalledTimes(1);
+});
+
+it(`should stop video if click play button`, () => {
+  const video = {
+    onloadeddata: null,
+    ontimeupdate: null,
+    play: jest.fn(),
+    pause: jest.fn(),
+    requestFullscreen: jest.fn()
+  };
+
+  const onExit = jest.fn();
+  const onActiveStatusChange = jest.fn();
+
+  const videoPlayer = mount(<VideoPlayer name={`Test`} poster={``} onExit={onExit} isActive={true} onActiveStatusChange={onActiveStatusChange}/>);
+  videoPlayer.instance().videoRef.current = video;
+
+  videoPlayer.find(`.player__play`).simulate(`click`);
+  expect(video.pause).toHaveBeenCalledTimes(1);
+  expect(onActiveStatusChange).toHaveBeenCalledTimes(1);
 });
